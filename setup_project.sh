a31ac870ba510845ef8eb08a2973299bf939399c
Shania Tinkouree <b.tinkouree@alustudent.com>
2026-04-12 23:24:04 +0400

    My shell script that automates the creation of the workspace, configures settings via the command line, and handles system signals gracefully.

---
+++ setup_project.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# 1. Input first
+read -p "Enter an input: " input
+
+# 2. Trap early (so it works anytime)
+trap '
+echo "Interrupted! Cleaning up..."
+
+tar -czf attendance_tracker_${input}_archive attendance_tracker_${input}
+
+rm -rf attendance_tracker_${input}
+
+exit
+' SIGINT
+
+
+# 3. Directory architecture
+
+mkdir attendance_tracker_${input}
+
+cd attendance_tracker_${input}
+
+mkdir Helpers reports
+
+
+# 4. Copy files
+
+cp ../project/attendance_checker.py .
+cp ../project/assets.csv Helpers/
+cp ../project/config.json Helpers/
+cp ../project/reports.log reports/
+
+
+# 5. Dynamic Configuration
+
+read -p "Do you want to update thresholds? (yes/no)" answer
+
+if [ "$answer" = "yes" ]; then
+	read -p "Enter your new warning value: " warning_value
+	read -p "Enter your new failure value: " failure_value
+
+	sed -i "s/warning/${warning_value}/" Helpers/config.json
+	sed -i "s/failure/${failure_value}/" Helpers/config.json
+fi
+
+
+# 6. Environment Validation
+
+echo "Checking Python installation..."
+
+if python3 --version >/dev/null 2>&1; then 
+	echo "Python3 is installed successfully."
+else 
+	echo "Warning: Python3 is NOT installed."
+fi
